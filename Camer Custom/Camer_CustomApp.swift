527e40224fea4368b4afe6fdc52faeaf91aaf561
hosam <hosamelmalt@gmail.com>
2020-12-14 07:21:16 +0200

    Initial Commit

---
+++ Camer Custom/Camer_CustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camer_CustomApp.swift
+//  Camer Custom
+//
+//  Created by hosam on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct Camer_CustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
